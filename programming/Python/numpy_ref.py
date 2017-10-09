30f716601d30282fe52927e20ae8d8a1e69a4d7c
SUNG MIN YANG 양 성 민 <sungmin.nlp@gmail.com>
2017-10-09 05:24:13 -1100

    Update numpy_ref.py

--- programming/Python/numpy_ref.py
+++ programming/Python/numpy_ref.py
@@ -1,4 +1,4 @@
-#1D->2D, other case, just use T, transpose.
+#1 . 1D->2D, other case, just use T, transpose.
 
 arr.reshape(ar.shape[0], -1)
 #array([ 0.80203158,  0.25762844,  0.67039516,  0.31021513,  0.80701097])
@@ -8,3 +8,15 @@ arr.reshape(ar.shape[0], -1)
 #       [ 0.67039516],
 #       [ 0.31021513],
 #       [ 0.80701097]])
+
+#2. argmax, argmax_top_N
+
+import numpy as np
+aa = [1,2,3,2,2,2,6,8,29,20,23,5]
+np.argmax(aa)
+#>> 8
+
+aa2 = np.argsort(aa)
+#aa2.artsort()[-N:][::-1]
+aa2.artsort()[-3:][::-1]
+#>> [8, 10, 9]
