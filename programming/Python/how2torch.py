56c7f6ace74ad18496a49eeb42557a2dc2ef5545
SUNG MIN YANG 양 성 민 <sungmin.nlp@gmail.com>
2018-04-04 15:13:34 +0200

    Update how2torch.py

--- programming/Python/how2torch.py
+++ programming/Python/how2torch.py
@@ -24,7 +24,7 @@ target_array = target_array.permute(0,1,2).continguous()
 target_array = torch.autograd.Variable(torch.randn(5,10,3)).t()             # target.size()==10,5,3  # torch.randn is FloatTensor
 target_array = torch.autograd.Variable(torch.randn(5,10,3)).permute(1,0,2)  # target.size()==10,5,3 
 # Both of them are not contiguous, simply change by one line.
-target_array.contiguous()
+target_array.contiguous() # does not increase performance dramatically, maybe 1-2%
 
 
 # Issue with fast/slow pack/unpack
@@ -39,3 +39,56 @@ word_dict["index2word"][index]   = "<pad>"
 # Add one more index-weights in embedding state_dict
 # > Either : pretrained_state_dict["encoder"]["weight"] += [Embedding_dim_weights] 
 # > OR     : pretrained_state_dict.encoder.weight.append([Embedding_dim_weights])
+
+
+
+# How to use multi model
+
+# Make multi models
+model_agnews = RNNModel(rnn_type, ntoken, ninp, nhid, nlayers, nclass, nhid_linear,
+                 dropout, dropouth, dropouti, dropoute, wdrop, tied, pooling,
+                 batch_norm, dropout_additional, relu)
+
+model_wiki = RNNModel(rnn_type, ntoken, ninp, nhid, nlayers, nclass, nhid_linear,
+                 dropout, dropouth, dropouti, dropoute, wdrop, tied, pooling,
+                 batch_norm, dropout_additional, relu)
+model_cat  = CatModel(input_dim, nclass)
+
+# Load weights for models.
+if from_scratch:
+    print("Learn from scratch!")
+elif not from_scratch :
+    load_pretrained_model(model_agnews, pretrained_dict_agnews) # e.g., file_path = "smAWD.pt"
+    load_pretrained_model(model_wiki, pretrained_dict_wiki) # e.g., file_path = "smAWD.pt"
+    
+    
+
+# Make one or more optimizer for multi models.
+if adam:
+    optimizer = optim.Adam(list(filter(lambda p: p.requires_grad, model_agnews.parameters()))+ 
+                           list(filter(lambda p: p.requires_grad, model_wiki.parameters()))+
+                           list(filter(lambda p: p.requires_grad, model_cat.parameters())),
+                           betas=(0.7, 0.9), lr=learning_rate, weight_decay=weight_decay)
+else:
+    optimizer = optim.SGD(list(filter(lambda p: p.requires_grad, model_agnews.parameters()))+ 
+                           list(filter(lambda p: p.requires_grad, model_wiki.parameters()))+
+                           list(filter(lambda p: p.requires_grad, model_cat.parameters())),
+                           lr=learning_rate, weight_decay=weight_decay)
+    
+  
+
+# Foward, backward, updates Loss
+output, hidden = model_agnews(seq_tensor, hidden)
+
+output = model_cat(output_from_models, nclass)    
+criterion = nn.CrossEntropyLoss()
+loss = criterion(output, target)
+
+# ============ grad udpate ================
+optimizer.zero_grad()
+loss.backward()
+clip = 0.01 # gradient clip
+torch.nn.utils.clip_grad_norm(model.parameters(), clip)
+optimizer.step()
+# ============ grad udpate ================
+        
