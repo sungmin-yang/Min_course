5e87672b1ee151216cdf2fff06ae75966f50ba21
SUNG MIN YANG 양 성 민 <sungmin.nlp@gmail.com>
2018-04-08 14:11:04 +0200

    Update how2file_process.py

--- programming/Python/how2file_process.py
+++ programming/Python/how2file_process.py
@@ -16,36 +16,36 @@ def get_label_dict(file_path, label_position=0):
 
 
 def make_labeled_file_per_class(input_file, output_file, instance_per_class=0, shuffle=True, complexity=20, label_position=0):
-    """
-    How to use :
-    file_path = "/Users/sungmin/Desktop/transfer_learning/Dataset/Seal-Software/41k_downsampled/2k_sample_provisions.txt"
-    trainloader = get_train_loader(file_path)
-    for
-    """
-    import numpy as np
+	"""
+	How to use :
+	file_path = "/Users/sungmin/Desktop/transfer_learning/Dataset/Seal-Software/41k_downsampled/2k_sample_provisions.txt"
+	trainloader = get_train_loader(file_path)
+	for
+	"""
+	import numpy as np
 
-    with open(input_file, 'r', encoding='utf8') as f:
-        for line in f:
-        	label = line.split()[label_position]
-            try : traindata_dict[label] += 1
-		    except : traindata_dict[label] == 1
+	with open(input_file, 'r', encoding='utf8') as f:
+		for line in f:
+			label = line.split()[label_position]
+			try : traindata_dict[label] += 1
+			except : traindata_dict[label] == 1
 
-		    if traindata_dict[label] >= instance_per_class:
-		    	continue			
+			if traindata_dict[label] >= instance_per_class:
+				continue			
 
 			traindata.append(line.rstrip('\n'))
 
 	if shuffle:
-	    for _ in range(complexity):
-	    	np.random.shuffle(traindata)
+		for _ in range(complexity):
+			np.random.shuffle(traindata)
  
 
-    with open(output_file, 'w', encoding='utf8') as f:
-    	for line in traindata:
-    		if not line.endswith('\n') : line+=' \n'
-    		f.write(line)
+	with open(output_file, 'w', encoding='utf8') as f:
+		for line in traindata:
+			if not line.endswith('\n') : line+=' \n'
+			f.write(line)
 
-    return print("Successfully make labeled file per class, per class {} instance exist".format(instance_per_class))
+	return print("Successfully make labeled file per class, per class {} instance exist".format(instance_per_class))
 
 
 def file_read_MultiProcess(file_path, process_func=False, num_workers=1, batch_size=4):
@@ -55,13 +55,13 @@ def file_read_MultiProcess(file_path, process_func=False, num_workers=1, batch_s
 		print("You did not provide process function, using defualt function which returns just line")
 		def process_fun(inputs):
 			return inputs
-    
-    cpu_num = mp.cpu_count()
-    print("You have {} CPU avaiable. You chose {} CPU for processing".format(cpu_num, num_workers))
+	
+	cpu_num = mp.cpu_count()
+	print("You have {} CPU avaiable. You chose {} CPU for processing".format(cpu_num, num_workers))
 
-    pool = mp.Pool(num_workers)
-    with open(file_path, 'r', encoding='utf8') as source_file:
-    	results = pool.map(process_line, source_file, batch_size)
+	pool = mp.Pool(num_workers)
+	with open(file_path, 'r', encoding='utf8') as source_file:
+		results = pool.map(process_line, source_file, batch_size)
 	pool.close()
 
 	return result
